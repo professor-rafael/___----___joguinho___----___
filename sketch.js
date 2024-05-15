84b1fd11c281cb5902e017b5acb8f64db423706d
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2024-05-15 11:20:41 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(51);
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(51);
+    rect(200,200,10,10)
+  }
+  // variáveis da cobrinha
+   let xCobra = 50;
+   let yCobra = 40;
+   let wCobra = 10;
+   let hCobra = 10;
+  
+  // velocidade da movimentação
+   let velocidadeMovimentacao = 8;
+  
+   function setup() {
+     createCanvas(1500, 400);
+   }
+  
+   function draw() {
+     background(1);
+     rect(xCobra, yCobra, wCobra, hCobra);
+  // adiciona comandos para as setinhas
+   if (keyIsDown(LEFT_ARROW)) {
+       xCobra -= velocidadeMovimentacao;
+     }
+   if (keyIsDown(RIGHT_ARROW)) {
+       xCobra += velocidadeMovimentacao;
+     }
+   if (keyIsDown(UP_ARROW)) {
+       yCobra -= velocidadeMovimentacao;
+     }
+   if (keyIsDown(DOWN_ARROW)) {
+       yCobra += velocidadeMovimentacao;
+     }
+  }
+  
